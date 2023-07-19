1a91810d619e83696cfe914a01193044ef11faf1
mirjSolution <ricojohndatoon23@gmail.com>
2023-07-19 19:44:39 -0400

    Apply clerk user management sign-in and sign-up

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
 const DashboardPage = () => {
-  return <p>Dashboard Page (Protected)</p>;
+  return (
+    <p>
+      Dashboard Page (Protected)
+      <UserButton afterSignOutUrl='/' />
+    </p>
+  );
 };
 
 export default DashboardPage;
